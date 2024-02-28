16ac861f8979a40617792fdafb1a20c3e2d899d3
HarrisonQAQ <92703206+SorrymakerTAT@users.noreply.github.com>
2024-02-28 16:58:37 +0800

    kinder-math-ques.php

--- kinder-math-ques.php
+++ kinder-math-ques.php
@@ -12,20 +12,16 @@
             <table>
 
             <?php
-            define('MIN', 1);
-            define('MAX', 9);
-            define('NUM_OF_QUES', 5);
-
-            for ($i = 0; $i < NUM_OF_QUES; $i++)  {
-
-                     /* 
+            /* 
 
                 Create constant variables for 'MIN', 'MAX' & 'NUM_OF_QUES'
 
                 Tips:
                 - PHP Constants [https://www.w3schools.com/php/php_constants.asp]
             */
-
+            define ('MIN',1);
+            define('MAX',9);
+            define('NUM_OF_QUES',6);
 
             /*
                 Create a for loop 
@@ -33,8 +29,11 @@
                 Tips:
                 - PHP for Loop [https://www.w3schools.com/php/php_looping_for.asp]
             */
-
-
+            for($i=1;$i<NUM_OF_QUES;$i++)
+            
+            {
+                
+                
                 /*
                     Create two variables and its values are the random number.
                     The random number must not less than 'MIN' and not more than 'MAX'
@@ -42,26 +41,25 @@
                     Tips:
                     - PHP rand() Function [https://www.w3schools.com/PHP/func_math_rand.asp]
                 */                
-                $n1 =  rand(MIN,MAX); // First number.
-                $n2 =  rand(MIN,MAX);// Second number.
-                $question_number = $i + 1;
-               echo '    <tr class="question">
-               <td>Q'.$question_number.'</td>
-               <td>'.$n1.' + '.$n2.' = </td>
-               <td>
-                   <input type="text" name="ans[]" maxlength="2" style="width: 2.0em" />
-                   <input type="hidden" name="nums1[]" value="'.$n1.'" />
-                   <input type="hidden" name="nums2[]" value="'.$n2.'" />
-               </td>
-           </tr>
-           <tr><td colspan="3"></td></tr>  ';
+                $n1 = rand(MIN,MAX); // First number.
+                $n2 = rand(MIN,MAX); // Second number.
 
+                
 
 
                 /*
                     Print the question and textbox. (Below is the HTML code to create a design like the question  mention, please do the modification in order to achieve the effect you want.)
                         ``` 
-                                                  
+                            <tr class="question">
+                                <td>Q1</td>
+                                <td>6 + 2 = </td>
+                                <td>
+                                    <input type="text" name="ans[]" maxlength="2" style="width: 2.0em" />
+                                    <input type="hidden" name="nums1[]" value="6" />
+                                    <input type="hidden" name="nums2[]" value="2" />
+                                </td>
+                            </tr>
+                            <tr><td colspan="3"></td></tr>                            
                         ```
                     
                     Hidden fields hold the first and second numbers.
@@ -71,7 +69,20 @@
                     Tips:
                     - PHP printf() Function [https://www.w3schools.com/php/func_string_printf.asp]
                 */
-                
+                printf('
+                        <tr class="question">
+                        <td>Q%d.</td>
+                        <td>%d+%d=</td>
+                        <td>
+                        <input type="text name="ans[]" maxlength="2" style="width:2.0em"/>
+                        <input type="hidden" name="nums1[]" value="%d"/>
+                        <input type="hidden" name="nums2[]" value="%d"/>
+                        </td>
+                        </tr>
+                        <tr>
+                        <td colspan="3"></td>
+                        </tr>',
+                        $i,$n1,$n2,$n1,$n2);
                 
 
             }
@@ -88,7 +99,7 @@
                 - PHP Syntax [https://www.w3schools.com/Php/php_syntax.asp]
              -->
 
-            <input type="button" value="Re-Generate" onclick="location='?'"/>
+            <input type="button" value="Re-Generate" onclick="location=' <?php echo $_SERVER['PHP_SELF']?>'"/>
         </form>
     </body>
 
